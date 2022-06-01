dc6bf7637da282a94db83b5c8342fd8b4f7f85e7
tunalad <novica.arsic@aol.com>
2022-06-01 23:56:53 +0200

    Scrapping job tags from page and putting them into an array

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python
+from pprint import pprint
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+def get_soup(url):
+    result = requests.get(url)
+    return bs(result.content, "html.parser")
+
+
+def get_job_tags_array(soup):
+    tags = soup.find_all("button", class_="btn btn-xs btn-primary jobtag __jobtag w-auto")
+    print("Total tag count:", len(tags))
+
+    list = []
+    for tag in tags:
+        list.append(tag.span.string)
+
+    return list
+
 
 if __name__ == "__main__":
-    print("Hello world!")
+    search, senioritet = "python", "0"
+    URL = f"https://www.helloworld.rs/oglasi-za-posao?q={search}&scope=full&senioritet[0]={senioritet}"
+
+    doc = get_soup(URL)
+    tags_list = get_job_tags_array(doc)
+
+    pprint(tags_list)
