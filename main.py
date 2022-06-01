9822b27cab17e49490cef93b8d53440faabefd56
tunalad <novica.arsic@aol.com>
2022-06-02 00:17:09 +0200

    Recursively checking other page's data

--- main.py
+++ main.py
@@ -12,20 +12,29 @@ def get_soup(url):
 
 def get_job_tags_array(soup):
     tags = soup.find_all("button", class_="btn btn-xs btn-primary jobtag __jobtag w-auto")
-    print("Total tag count:", len(tags))
+    print("Total tag count on this page:", len(tags))
 
     list = []
     for tag in tags:
         list.append(tag.span.string)
 
+    page_next = get_next_page(soup)
+
+    if page_next != None:
+        soup2 = get_soup(page_next)
+        list += get_job_tags_array(soup2)
+    else:
+        print("That all of the pages")
+
     return list
 
 
-def get_next_page():
+def get_next_page(soup):
     try:
-        pages_list = doc.find("div", class_="flex items-center justify-center gap-3 md:gap-4 pagination")
+        pages_list = soup.find("div", class_="flex items-center justify-center gap-3 md:gap-4 pagination")
         btn_next = pages_list.find("i", class_="las la-angle-right text-lg").parent
 
+        print("https://www.helloworld.rs" + btn_next['href'])
         return "https://www.helloworld.rs" + btn_next['href']
     except:
         return None
@@ -37,4 +46,4 @@ if __name__ == "__main__":
     doc = get_soup(URL)
     tags_list = get_job_tags_array(doc)
 
-    print(get_next_page())
+    pprint("Total tags count (including duplicates):", len(tags_list))
