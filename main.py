7a554e6d2b7fc767bacaf1194d3fd9aeaae30ab7
tunalad <28594843+tunalad@users.noreply.github.com>
2025-02-01 19:56:46 +0100

    fixed get_next_page, formatting & other minor stuff
    they yet again changed the next button styling. I should probably look
    for a different solution

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-from pprint import pprint
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
 
+from urllib.parse import quote
 import requests
 from bs4 import BeautifulSoup as bs
 from matplotlib import pyplot
-from urllib.parse import quote
 
 
 def get_soup(url):
@@ -13,27 +14,34 @@ def get_soup(url):
 
 
 def get_job_tags_array(soup):
-    tags = soup.find_all("a", class_="btn btn-xs btn-primary w-auto jobtag __jobtag __ga4_job_tech_tag")
+    tags = soup.find_all(
+        "a", class_="btn btn-xs btn-primary w-auto jobtag __jobtag __ga4_job_tech_tag"
+    )
 
-    list = []
+    tags_list = []
     for tag in tags:
-        list.append(tag.text)
+        tags_list.append(tag.text)
 
     page_next = get_next_page(soup)
 
-    if page_next != None:
+    if page_next is not None:
         soup2 = get_soup(page_next)
-        list += get_job_tags_array(soup2)
+        tags_list += get_job_tags_array(soup2)
 
-    return list
+    return tags_list
 
 
 def get_next_page(soup):
     try:
-        pages_list = soup.find("div", class_="flex items-center justify-center gap-3 md:gap-4 pagination")
-        btn_next = pages_list.find("i", class_="las la-angle-right text-lg").parent
+        pages_list = soup.find(
+            "div", class_="flex items-center justify-center gap-3 md:gap-4 pagination"
+        )
+        btn_next = pages_list.find(
+            "i", class_="las la-angle-right text-lg pagination-new-design"
+        ).parent
 
-        return "https://www.helloworld.rs" + btn_next['href']
+        print("https://www.helloworld.rs" + btn_next["href"])
+        return "https://www.helloworld.rs" + btn_next["href"]
     except:
         return None
 
@@ -54,41 +62,36 @@ def draw_plot(x, y):
 
 def input_handler(text=""):
     text_in = input(text)
-    if text_in.isspace() or text_in == "":
-        exit("Invalid input")
-    else:
-        return text_in.replace(" ", "-")
+    return text_in.replace(" ", "-")
 
 
-if __name__ == "__main__":
+def main():
     search = input_handler("Search by keyword: ")
-    search = quote(search)
-    senioritet = input_handler("""Seniority
+    search = f"q={quote(search)}&"
+    senioritet = input_handler(
+        """Seniority
     1 - junior
     2 - intermediate
     3 - senior
     (other numbers will ignore seniority filtering)
-    >: """)
+    >: """
+    )
 
-
-    URL = f"https://www.helloworld.rs/oglasi-za-posao?q={search}&scope=full&senioritet[0]={senioritet}"
+    url = f"https://www.helloworld.rs/oglasi-za-posao?{search}scope=full&senioritet[0]={senioritet}"
 
     # scrapping website data
-    doc = get_soup(URL)
+    doc = get_soup(url)
     tags_list = get_job_tags_array(doc)
 
     # organizing data into a dictionary
     tags_dict = []
     for tag in list(dict.fromkeys(tags_list)):
-        tags_dict.append({
-            'tag': tag,
-            'count': int(tags_list.count(tag))
-        })
+        tags_dict.append({"tag": tag, "count": int(tags_list.count(tag))})
 
     print("Scrapping tags, please wait")
 
     print(len(tags_list), "tags counted")
-    print(f"Tags found for {search}:", len(tags_dict))
+    print(f'Tags found for "{search[2:-1]}":', len(tags_dict))
 
     get_bars = int(input_handler("Display how many first tags?: "))
 
@@ -96,13 +99,17 @@ if __name__ == "__main__":
         exit("Invalid input")
 
     # sorting data (from most to least)
-    tags_dict = sorted(tags_dict, key=lambda x: x['count'], reverse=True)
+    tags_dict = sorted(tags_dict, key=lambda x: x["count"], reverse=True)
 
     # regrouping data for plotting
     tag_names = []
     tag_counts = []
     for t in range(len(tags_dict[:get_bars])):
-        tag_names.append(tags_dict[t].get('tag'))
-        tag_counts.append(tags_dict[t].get('count'))
+        tag_names.append(tags_dict[t].get("tag"))
+        tag_counts.append(tags_dict[t].get("count"))
 
     draw_plot(tag_names, tag_counts)
+
+
+if __name__ == "__main__":
+    main()
